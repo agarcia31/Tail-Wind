63408bc3de6caf85e87229cc9361beaa9bebd1dd
Andy <a.garcia.atn31@gmail.com>
2023-02-22 11:53:19 -0800

    begining of learning tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      'font-size': {
+        '8xl': '8rem'
+      },
+      'colors': {
+        'red-custom': '#fd681b'
+      }
+    },
+  },
+  plugins: [],
+}
